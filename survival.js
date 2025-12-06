9a812be42dbcd9b7fa39164aef3cf51be6e74b46
skmwx <79213055+skmwx@users.noreply.github.com>
2025-12-06 13:42:23 +0100

    Display player stats

--- survival.js
+++ survival.js
@@ -15,3 +15,16 @@ class Base {
 
 const player = new Player();
 const base = new Base();
+
+function refreshPlayer() {
+  const healthValueNode = document.getElementById('healthValue');
+  const hungerValueNode = document.getElementById('hungerValue');
+
+  if (healthValueNode) {
+    healthValueNode.textContent = player.health;
+  }
+
+  if (hungerValueNode) {
+    hungerValueNode.textContent = player.hunger;
+  }
+}
